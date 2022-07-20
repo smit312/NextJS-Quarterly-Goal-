dd6abc3cbaa3cd6342a3bbea1ac79580e4b1a299
smit312 <smit.coder31@gmail.com>
2022-07-20 14:43:51 +0530

    url

--- Quarterly-Goal/1/next-crash-course/config/index.js
+++ Quarterly-Goal/1/next-crash-course/config/index.js
@@ -1,3 +1,3 @@
 export const server = !process.env.NEXT_DEV
   ? "http://localhost:3000"
-  : "https://next-js-quarterly-goal-1.vercel.app";
+  : "https://next-js-quarterly-goal-1-fekvpiw2o-smitcoder31-gmailcom.vercel.app";
