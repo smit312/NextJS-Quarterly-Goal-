8031777540e6b2083102399e9a97f33cd4ecefeb
smit312 <smit.coder31@gmail.com>
2022-07-20 15:53:43 +0530

    removed JSON.stringify

--- Quarterly-Goal/1/next-crash-course/config/index.js
+++ Quarterly-Goal/1/next-crash-course/config/index.js
@@ -1,3 +1,5 @@
-export const server = !process.env.NEXT_DEV
-  ? "http://localhost:3000"
-  : "https://next-js-quarterly-goal-1-fekvpiw2o-smitcoder31-gmailcom.vercel.app";
+const dev = process.env.NEXT_DEV !== "production";
+
+export const server = process.env.NEXT_DEV
+  ? "http://localhost:3001"
+  : "https://yourwebsite.com";
