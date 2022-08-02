f1ce86683f1457c5d0595c3fca20f060651bdee3
smit312 <smit.coder31@gmail.com>
2022-08-02 18:00:37 +0530

    added style in components

--- Quarterly-Goal/3/nextjs-course/components/events/event-item.js
+++ Quarterly-Goal/3/nextjs-course/components/events/event-item.js
@@ -1,27 +1,31 @@
 import Link from "next/link";
+import classes from "./event-item.module.css";
+
 function EventItem(props) {
   const { title, image, date, location, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "long",
-    year: numeric,
+    year: "numeric",
   });
   const formattedAddress = location.replace(", ", "/n");
   const exploreLink = `/events/${id}`;
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <h2>{title}</h2>
-        <div>
-          <time>{humanReadableDate}</time>
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress} </address>
+          </div>
         </div>
-        <div>
-          <address>{formattedAddress} </address>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
         </div>
-      </div>
-      <div>
-        <Link href={exploreLink }>Explore Event</Link>
       </div>
     </li>
   );
