598bfc71b35f3a5729e2842abd2ca440802b7533
smit312 <smit.coder31@gmail.com>
2022-08-02 17:24:37 +0530

    Adding More React Components & Connecting Components

--- Quarterly-Goal/3/nextjs-course/components/events/event-item.js
+++ Quarterly-Goal/3/nextjs-course/components/events/event-item.js
@@ -1,5 +1,30 @@
+import Link from "next/link";
 function EventItem(props) {
-    return
+  const { title, image, date, location, id } = props;
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: numeric,
+  });
+  const formattedAddress = location.replace(", ", "/n");
+  const exploreLink = `/events/${id}`;
+  return (
+    <li>
+      <img src={"/" + image} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <div>
+          <time>{humanReadableDate}</time>
+        </div>
+        <div>
+          <address>{formattedAddress} </address>
+        </div>
+      </div>
+      <div>
+        <Link href={exploreLink }>Explore Event</Link>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem;
--- Quarterly-Goal/3/nextjs-course/pages/index.js
+++ Quarterly-Goal/3/nextjs-course/pages/index.js
@@ -1,11 +1,12 @@
 import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
 function HomePage() {
-  const FeaturedEvents = getFeaturedEvents
+  const FeaturedEvents = getFeaturedEvents;
   return (
     <div>
       <h1> Home Page </h1>
-      
+      <EventList items={FeaturedEvents} />
     </div>
   );
 }
