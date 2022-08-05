cff25786764bff62cbae06bdece68e75b62997be
smit312 <smit.coder31@gmail.com>
2022-08-05 18:00:26 +0530

    Worked on the FilterdEvent Page

--- Quarterly-Goal/3/nextjs-course/pages/events/index.js
+++ Quarterly-Goal/3/nextjs-course/pages/events/index.js
@@ -1,12 +1,21 @@
+import { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
-function AllEventsPage() {
-  const events = getAllEvents();
+import EventSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 
+function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
-    <div>
+    <Fragment>
+      <EventSearch onSearch={findEventHandler} />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
 export default AllEventsPage;
