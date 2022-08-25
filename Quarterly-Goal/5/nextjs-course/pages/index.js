99146015fca42412ed1de56fc2fe97459c04ef6d
smit312 <smit.coder31@gmail.com>
2022-08-25 10:16:22 +0530

    configured the 'head' content

--- Quarterly-Goal/5/nextjs-course/pages/index.js
+++ Quarterly-Goal/5/nextjs-course/pages/index.js
@@ -1,10 +1,17 @@
-// import { getFeaturedEvents } from "../dummy-data";
+import Head from "next/head";
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to envolve..."
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
