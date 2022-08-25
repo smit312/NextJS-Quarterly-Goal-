c4665d1e6666be48ee1efd7191f877c10514427e
smit312 <smit.coder31@gmail.com>
2022-08-25 11:33:10 +0530

    add 'head' content in _app.js file

--- Quarterly-Goal/5/nextjs-course/pages/_app.js
+++ Quarterly-Goal/5/nextjs-course/pages/_app.js
@@ -1,9 +1,18 @@
+import Head from "next/head";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name="description" content="NextJs Events" />
+        <meta
+          name="viewport"
+          content="initial-scale=0.1 , width=device-width"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
