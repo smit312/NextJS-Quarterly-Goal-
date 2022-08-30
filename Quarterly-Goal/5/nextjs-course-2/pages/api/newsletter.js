42e7bdb3243979bc34b1cc2798ba9b38a3c94840
smit312 <smit.coder31@gmail.com>
2022-08-30 12:54:37 +0530

    setup the mongodb connection and add the query inside newsletter route

--- Quarterly-Goal/5/nextjs-course-2/pages/api/newsletter.js
+++ Quarterly-Goal/5/nextjs-course-2/pages/api/newsletter.js
@@ -1,12 +1,20 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
     if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
-    console.log(userEmail);
-    res.status(201).json({message:'Signed up!'})
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://smit:smit@cluster0.y641vhb.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+    client.close();
+
+    res.status(201).json({ message: "Signed up!" });
   }
 }
 
