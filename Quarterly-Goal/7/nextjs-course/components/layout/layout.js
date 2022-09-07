b3286bcf8f0871adbc5f474b3b46c5290d265760
smit312 <smit.coder31@gmail.com>
2022-09-07 16:00:07 +0530

    added layout and navigation

---
+++ Quarterly-Goal/7/nextjs-course/components/layout/layout.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import MainNavigation from "./main-navigation";
+function layout(props) {
+  return (
+    <Fragment>
+      <MainNavigation />
+      <main>{props.children}</main>
+    </Fragment>
+  );
+}
+export default layout;
