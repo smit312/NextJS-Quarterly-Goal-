64627bc6486d68c7f91886f42eac4a00a501cf38
smit312 <smit.coder31@gmail.com>
2022-09-08 16:10:46 +0530

    added post grid and post items

---
+++ Quarterly-Goal/7/nextjs-course/components/posts/post-item.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./post-item.module.css";
+function PostItem(props) {
+  const { title, time, image, excerpt, date, slug } = props.post;
+  const formatedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formatedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+}
+export default PostItem;
