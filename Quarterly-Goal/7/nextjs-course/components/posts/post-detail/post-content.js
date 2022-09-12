135d9db416d33a47009249f67b42ab2ba0700d15
smit312 <smit.coder31@gmail.com>
2022-09-12 16:13:51 +0530

    added markdown file as a data source

--- Quarterly-Goal/7/nextjs-course/components/posts/post-detail/post-content.js
+++ Quarterly-Goal/7/nextjs-course/components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 const DUMMY_POST = {
@@ -5,14 +6,15 @@ const DUMMY_POST = {
   title: "Getting started with nextJs",
   image: "getting-started-nextjs.png",
   date: "2022-02-10",
-  content: "# this is a first post",
+  content: "## this is a first post",
 };
 const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
 function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
---
+++ Quarterly-Goal/7/nextjs-course/content/posts/getting-started-with-nextjs.md
@@ -0,0 +1,7 @@
+---
+title : Getting Started with NextJs 
+date : '2022-10-15'
+image : getting-started-nextjs.png                                  
+excerpt : "nextJs is a the React framework for production - it makes building fullstack react apps and site a breeze and ships with built-in SSR."
+isFeatured : true
+---
