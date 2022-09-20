9135362a192bac17c1f876de002299c4fb2fb82c
smit312 <smit.coder31@gmail.com>
2022-09-20 17:59:05 +0530

    worked on the posts

--- Quarterly-Goal/7/nextjs-course/components/posts/post-detail/post-content.js
+++ Quarterly-Goal/7/nextjs-course/components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
@@ -5,11 +6,23 @@ import classes from "./post-content.module.css";
 function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customRenderers = {
+    image(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          height={image.alt}
+          width={600}
+          alt={300}
+        />
+      );
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
 
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
